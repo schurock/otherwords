49556d3b3fc8e5424dfc74de7bb359de3ded4fd7
Tim Schurig <t.schurig@gmail.com>
2015-12-19 11:42:08 +0000

    add home, help and about page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select 'title', 'home | otherwords'
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select 'title', 'help | otherwords'
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select 'title', 'about | otherwords'
+  end
 end
